868e89afcf23a68ca4ab6ce4c8c54bfed37c208e
McGilfordJose <chucho970407@gmail.com>
2021-01-30 01:02:34 -0600

    P3 Jose

--- Postwork 3/P3.R
+++ Postwork 3/P3.R
@@ -10,4 +10,15 @@
 #Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo visitante.
 #Un HeatMap para las probabilidades conjuntas estimadas de los números de goles que anotan el equipo de casa y el equipo visitante en un partido.
 
+#install.packages("ggplot2")
+library(ggplot2)
 
+
+
+#Jose Grafico 3
+ggplot(mtcars, aes(x=cyl, y = hp, colour = mpg )) + 
+  geom_point() +   
+  theme_gray() +   # Temas (inteta cambiarlo)
+  facet_wrap("cyl") +  # Lo divide por el núm de cilindros
+  xlab('Núm Acilindros')+  # Nombre en los ejes
+  ylab('Caballos de Fuerza')
