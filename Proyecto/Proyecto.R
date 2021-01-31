59d0805f8f7016e3ddc1f9f12725ecea6f4f02d6
McGilfordJose <chucho970407@gmail.com>
2021-01-31 02:01:35 -0600

    Jos

--- Proyecto/Proyecto.R
+++ Proyecto/Proyecto.R
@@ -14,8 +14,7 @@ class(datos)
 summary(datos)
 dim(datos)
 
-datos %>%  group_by(Año) %>% 
-  +   summarise(mean = mean(amount), sum = sum(amount), n = n())
+datos %>%  group_by(Año) %>% summarise(mean = mean(Población.total), sum = sum(Población.de.15.años.y.más), n = n())
 datos
 View(datos)
 names(datos)
