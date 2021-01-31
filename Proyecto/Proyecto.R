e7d2993048e6e313d3bf0f980d19571deee5c601
McGilfordJose <chucho970407@gmail.com>
2021-01-30 19:53:24 -0600

    Proyec

--- Proyecto/Proyecto.R
+++ Proyecto/Proyecto.R
@@ -4,8 +4,14 @@ getwd()
 #setwd("BEDU/DataScience/Postworks/Poswork 1/BEDU/Proyecto/")
 
 #install.packages("dplyr")
+#install.packages("ggplot2")
 library(dplyr)
+library(ggplot2)
 
 datos<-read.csv("Consolidado para R-ENOE-Indicadores estratégicos-enero2005-diciembre2020.csv")
 class(datos)
 summary(datos)
+dim(datos)
+
+datos<-na.omit(datos)
+datos
