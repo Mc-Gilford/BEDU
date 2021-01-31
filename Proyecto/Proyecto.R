12aa88b892c36b1b309b8444c9399bda7a45899b
McGilfordJose <chucho970407@gmail.com>
2021-01-30 18:00:48 -0600

    Pro

--- Proyecto/Proyecto.R
+++ Proyecto/Proyecto.R
@@ -1,1 +1,7 @@
-install.packages("dployr")
+#Obtenemos la ruta de donde nos ubicamos
+getwd()
+#Colocamos la ruta a donde iremos
+#setwd("BEDU/DataScience/Postworks/Poswork 1/BEDU/Proyecto/")
+
+#install.packages("dplyr")
+library(dplyr)
